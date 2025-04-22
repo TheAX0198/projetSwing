c591a36d6641afb4a63e8b4975e7322de09731b0
TheAX0198 <xguim68@gmail.com>
2025-04-21 22:14:48 -0400

    Ajout/Correctif backgrounds
    1. Nouveau background pour PanelControle et PanelListe
    
    2. Ajustement du background graphique pour un meilleur esthétique RGB

--- src/pkgComposants/pkgPanelBoutons/PanelControle.java
+++ src/pkgComposants/pkgPanelBoutons/PanelControle.java
@@ -3,13 +3,19 @@ package pkgComposants.pkgPanelBoutons;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 
 public class PanelControle extends JPanel {
 
     private TitledBorder bordure;
+    private String bgImg;
 
     public PanelControle() {
         this.bordure = new TitledBorder(null, "Contrôles", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font(Font.SANS_SERIF, Font.BOLD, 13), Color.BLACK);
@@ -19,13 +25,26 @@ public class PanelControle extends JPanel {
         setLumineux();
     }
 
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Dessiner l'arrière-plan
+        Image bg = new ImageIcon("res/background/" + bgImg + ".png").getImage();
+        g2d.drawImage(bg, 0, 0, null);
+
+    }
+
     public void setSombre() {
         this.setBackground(Color.BLACK);
+        bgImg = "bg_dark_controle";
         bordure.setTitleColor(Color.WHITE);
     }
 
     public void setLumineux() {
         this.setBackground(Color.WHITE);
+        bgImg = "bg_bright_controle";
         bordure.setTitleColor(Color.BLACK);
     }
 
--- src/pkgComposants/pkgPanelListe/PanelListe.java
+++ src/pkgComposants/pkgPanelListe/PanelListe.java
@@ -3,13 +3,18 @@ package pkgComposants.pkgPanelListe;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 
 public class PanelListe extends JPanel {
 
     private TitledBorder bordure;
+    private String bgImg;
 
     public PanelListe() {
         this.bordure = new TitledBorder(null, "Liste et checkboxes", TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, new Font(Font.SANS_SERIF, Font.BOLD, 13), Color.BLACK);
@@ -19,13 +24,26 @@ public class PanelListe extends JPanel {
         setLumineux();
     }
 
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Dessiner l'arrière-plan
+        Image bg = new ImageIcon("res/background/" + bgImg + ".png").getImage();
+        g2d.drawImage(bg, 0, 0, null);
+
+    }
+
     public void setSombre() {
         this.setBackground(Color.BLACK);
+        bgImg = "bg_dark_liste";
         bordure.setTitleColor(Color.WHITE);
     }
 
     public void setLumineux() {
         this.setBackground(Color.WHITE);
+        bgImg = "bg_bright_liste";
         bordure.setTitleColor(Color.BLACK);
     }
 
